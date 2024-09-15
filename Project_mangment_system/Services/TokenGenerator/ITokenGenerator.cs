77d5c4397fb6e7df12c130c0e7c90adddd1b695d
Mamn847 <133536689+Mamn847@users.noreply.github.com>
2024-09-15 18:35:45 +0300

    test

--- Project_mangment_system/Services/TokenGenerator/ITokenGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Project_management_system.Models;
-
-namespace Project_management_system.Services.TokenGenerator
-{
-    public interface ITokenGenerator
-    {
-        string GenerateToken(User user);
-    }
-}
